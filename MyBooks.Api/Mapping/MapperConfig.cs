abe19343f967d13faea04df418c5fed7ae299728
Tóth Dániel <toth.dani92@hotmail.com>
2019-02-26 09:43:52 +0100

    Created the initial mapper configuration.

---
+++ MyBooks.Api/Mapping/MapperConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using AutoMapper;
+using MyBooks.Bll.Entities;
+
+namespace MyBooks.Api.Mapping
+{
+    public class MapperConfig
+    {
+        public static IMapper Configure()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                //cfg.AllowNullCollections = true;
+                cfg.CreateMap<Book, Dtos.Book>()
+                    .ForMember(dto => dto.Authors, opt => opt.Ignore())
+                    .ForMember(dto => dto.CoverUrl,
+                        opt => opt.MapFrom(src => $@"http://localhost:5000/covers/{src.CoverImagePath}"))
+                    .ForMember(dto => dto.GoodreadsUrl,
+                        opt => opt.MapFrom(src => $@"https://www.goodreads.com/book/show/{src.GoodreadsId}"));
+                cfg.CreateMap<Dtos.Book, Book>();
+
+                // TODO: CreateMap for other entities and dtos as well
+            });
+
+            var mapper = config.CreateMapper();
+            return mapper;
+        }
+    }
+}
