8426a440a4487e22c185054a23e153dda4a27a39
Daniel <toth.dani92@hotmail.com>
2019-04-18 10:39:47 +0200

    Added new project for IdentityServer4

---
+++ MyBooks.IdentityServer/Config.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace MyBooks.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("myBooksApi", "MyBooks API")
+                {
+                    ApiSecrets = { new Secret("secret".Sha256()) }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                // Native clients
+                new Client
+                {
+                    ClientId = "native.hybrid",
+                    ClientName = "Native Client (Hybrid with PKCE)",
+
+                    RedirectUris = {"https://notused"},
+                    PostLogoutRedirectUris = {"https://notused"},
+
+                    RequireClientSecret = false,
+
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RequirePkce = true,
+                    AllowedScopes = {"openid", "profile", "email", "myBooksApi"},
+
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse
+                }
+            };
+        }
+    }
+}
