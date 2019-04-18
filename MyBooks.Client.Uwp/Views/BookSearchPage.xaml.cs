bd25de471720f8ec4fe0cc5db591886fae68558f
Daniel <toth.dani92@hotmail.com>
2019-04-18 10:53:44 +0200

    Added new Page for searching books

---
+++ MyBooks.Client.Uwp/Views/BookSearchPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Reactive.Disposables;
+using Windows.UI.Xaml.Controls;
+using MyBooks.Client.ViewModels;
+using ReactiveUI;
+using Splat;
+
+namespace MyBooks.Client.Uwp.Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class BookSearchPage : Page, IViewFor<BookSearchViewModel>
+    {
+        public BookSearchPage()
+        {
+            this.InitializeComponent();
+
+            ViewModel = Locator.Current.GetService<BookSearchViewModel>();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.Results, v => v.bookResultsListBox.ItemsSource)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel, vm => vm.SearchTerm, v => v.searchTextBox.Text)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (BookSearchViewModel)value;
+        }
+
+        public BookSearchViewModel ViewModel
+        {
+            get => (BookSearchViewModel) DataContext;
+            set => DataContext = value;
+        }
+    }
+}
