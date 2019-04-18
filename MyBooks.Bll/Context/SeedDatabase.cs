06bfa299aae9cbb1a017b6fa069d1cc1ab360e69
Daniel <toth.dani92@hotmail.com>
2019-04-18 10:54:13 +0200

    Added to seed the database

---
+++ MyBooks.Bll/Context/SeedDatabase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using MyBooks.Bll.Entities;
+
+namespace MyBooks.Bll.Context
+{
+    public static class SeedDatabase
+    {
+        public static void Seed(this ApplicationDbContext context)
+        {
+            if (!context.Books.Any())
+            {
+                var book_wayofkings = new Book
+                {
+                    Title = "The Way Of Kings",
+                    CoverImagePath = "wayofkings.jpg",
+                    Genre = "Fantasy"
+                };
+                var book_redrising = new Book
+                {
+                    Title = "Red Rising",
+                    CoverImagePath = "redrising.jpg",
+                    Genre = "Science Fiction"
+                };
+                var book_theeyeoftheworld = new Book
+                {
+                    Title = "The Eye of the World",
+                    CoverImagePath = "theeyeoftheworld.jpg",
+                    Genre = "Fantasy"
+                };
+
+                context.Books.Add(book_wayofkings);
+                context.Books.Add(book_redrising);
+                context.Books.Add(book_theeyeoftheworld);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
