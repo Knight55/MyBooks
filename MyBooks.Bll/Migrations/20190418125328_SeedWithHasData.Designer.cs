c48264b7a98ccba1c3ca0da65d3dcacb6668a96c
Daniel <toth.dani92@hotmail.com>
2019-05-01 18:50:26 +0200

    New migration with HasData

--- MyBooks.Bll/Migrations/20190418125328_SeedWithHasData.Designer.cs
+++ MyBooks.Bll/Migrations/20190418125328_SeedWithHasData.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyBooks.Bll.Context;
 
 namespace MyBooks.Bll.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190418125328_SeedWithHasData")]
+    partial class SeedWithHasData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
