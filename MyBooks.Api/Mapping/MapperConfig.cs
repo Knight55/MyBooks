710c0b6fa925730325e39eb8008d4232cadc6286
Daniel <toth.dani92@hotmail.com>
2019-05-22 14:01:53 +0200

    Fixed mapping for cover and goodreads url

--- MyBooks.Api/Mapping/MapperConfig.cs
+++ MyBooks.Api/Mapping/MapperConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using AutoMapper;
 using MyBooks.Bll.Entities;
 
@@ -14,10 +15,18 @@ namespace MyBooks.Api.Mapping
                 cfg.CreateMap<Book, Dto.Dtos.Book>()
                     //.ForMember(dto => dto.Authors, opt => opt.Ignore())
                     .ForMember(dto => dto.CoverUrl,
-                        opt => opt.MapFrom(src => $@"http://localhost:5000/covers/{src.CoverImagePath}"))
+                        opt => opt.MapFrom(src =>
+                            $@"http://localhost:5000/covers/{src.CoverImagePath}"))
                     .ForMember(dto => dto.GoodreadsUrl,
-                        opt => opt.MapFrom(src => $@"https://www.goodreads.com/book/show/{src.GoodreadsId}"));
-                cfg.CreateMap<Dto.Dtos.Book, Book>();
+                        opt => opt.MapFrom(src =>
+                            $@"https://www.goodreads.com/book/show/{src.GoodreadsId}"));
+                cfg.CreateMap<Dto.Dtos.Book, Book>()
+                    .ForMember(entity => entity.CoverImagePath,
+                        opt => opt.MapFrom(src =>
+                            src.CoverUrl == null ? "" : src.CoverUrl.Substring(src.CoverUrl.LastIndexOf(@"/", StringComparison.Ordinal) + 1)))
+                    .ForMember(entity => entity.GoodreadsId,
+                        opt => opt.MapFrom(src =>
+                            src.GoodreadsUrl == null ? "" : src.GoodreadsUrl.Substring(src.GoodreadsUrl.LastIndexOf(@"/", StringComparison.Ordinal) + 1)));
 
                 // TODO: CreateMap for other entities and dtos as well
             });
