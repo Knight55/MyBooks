f19846ffc7e8746d6083338e4932eed573675355
Daniel <toth.dani92@hotmail.com>
2019-05-22 10:26:01 +0200

    Added WPF project to the solution

---
+++ MyBooks.Client.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyBooks.Client.ViewModels;
+using ReactiveUI;
+using Splat;
+
+namespace MyBooks.Client.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<AppViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new AppViewModel(Locator.Current.GetService<IScreen>());
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.Bind(ViewModel, vm => vm.HostScreen.Router, v => v.viewHost.Router)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnExit(object sender, RoutedEventArgs args)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ MyBooks.Client.Wpf/Views/BookSearchView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Controls;
+using MyBooks.Client.ViewModels;
+using MyBooks.Dto.Dtos;
+using ReactiveUI;
+
+namespace MyBooks.Client.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for BookSearchView.xaml
+    /// </summary>
+    public partial class BookSearchView : ReactiveUserControl<BookSearchViewModel>
+    {
+        public BookSearchView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.Results, v => v.bookResultsListBox.ItemsSource)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel, vm => vm.SearchTerm, v => v.searchTextBox.Text)
+                    .DisposeWith(disposableRegistration);
+
+                IObservable<EventPattern<SelectionChangedEventArgs>> selectionChangedObservable =
+                    Observable.FromEventPattern<SelectionChangedEventArgs>(bookResultsListBox, "SelectionChanged");
+
+                selectionChangedObservable
+                    .Subscribe(evt =>
+                    {
+                        var listBox = (ListBox)evt.Sender;
+                        var book = (Book)listBox?.SelectedItem;
+                        if (book != null)
+                        {
+                            Debug.WriteLine($"Clicked on book: {book?.Title}");
+                            ViewModel.GoToBookDetails.Execute(book).Subscribe();
+                        }
+                    });
+            });
+        }
+    }
+}
