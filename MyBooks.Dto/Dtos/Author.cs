91d7792bfa0013faf0c45fac6f5ee8f67ae0a0b9
Daniel <toth.dani92@hotmail.com>
2019-06-13 18:28:22 +0200

    New dto for Authors.

--- MyBooks.Dto/Dtos/Author.cs
+++ MyBooks.Dto/Dtos/Author.cs
@@ -1,7 +1,24 @@
-﻿namespace MyBooks.Dto.Dtos
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBooks.Dto.Dtos
 {
     public class Author
     {
-        
+        /// <summary>
+        /// The identifier of the author.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the author.
+        /// </summary>
+        [Required(ErrorMessage = "Author name is required", AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public List<int> BookIds { get; set; }
     }
 }
