3197593bce359c86bbd259d51766e928da5f6c63
Daniel <toth.dani92@hotmail.com>
2019-06-13 18:23:50 +0200

    Added service for Author.

---
+++ MyBooks.Bll/Services/AuthorService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using MyBooks.Bll.Context;
+using MyBooks.Bll.Entities;
+
+namespace MyBooks.Bll.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Author GetAuthor(int authorId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Author InsertAuthor(Author newAuthor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateAuthor(int authorId, Author updatedAuthor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteAuthor(int authorId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MyBooks.Bll/Services/IAuthorService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MyBooks.Bll.Entities;
+
+namespace MyBooks.Bll.Services
+{
+    public interface IAuthorService
+    {
+        Author GetAuthor(int authorId);
+        IEnumerable<Author> GetAuthors();
+        Author InsertAuthor(Author newAuthor);
+        void UpdateAuthor(int authorId, Author updatedAuthor);
+        void DeleteAuthor(int authorId);
+    }
+}
