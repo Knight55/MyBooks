9eda4dddbcde7046e2c52f7f138d6f892bb25bbd
Daniel <toth.dani92@hotmail.com>
2019-09-08 21:53:19 +0200

    Add alternate key.

--- MyBooks.Bll/Migrations/20190614084315_Edition_Alternate_Key.Designer.cs
+++ MyBooks.Bll/Migrations/20190614084315_Edition_Alternate_Key.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyBooks.Bll.Context;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyBooks.Bll.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190614084315_Edition_Alternate_Key")]
+    partial class Edition_Alternate_Key
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyBooks.Bll/Migrations/20190614084315_Edition_Alternate_Key.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyBooks.Bll.Migrations
+{
+    public partial class Edition_Alternate_Key : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsbnNumber",
+                table: "Editions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Editions_IsbnNumber",
+                table: "Editions",
+                column: "IsbnNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Editions_IsbnNumber",
+                table: "Editions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IsbnNumber",
+                table: "Editions",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
