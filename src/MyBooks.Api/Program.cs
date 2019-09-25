0dee4b9035512a824472a9d744f41f78ffe8f3a7
Daniel <toth.dani92@hotmail.com>
2019-09-25 11:18:03 +0200

    Moved projects to new folder structure

---
+++ src/MyBooks.Api/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MyBooks.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .ConfigureKestrel(serverOptions => { })
+                        .UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/MyBooks.Api/Startup.cs
@@ -0,0 +1,169 @@
+﻿using Hellang.Middleware.ProblemDetails;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyBooks.Api.Mapping;
+using MyBooks.Bll.Context;
+using MyBooks.Bll.Exceptions;
+using MyBooks.Bll.Services;
+using Newtonsoft.Json;
+
+namespace MyBooks.Api
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Logging
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConfiguration(Configuration.GetSection("Logging"));
+                loggingBuilder.AddConsole();
+            });
+
+            // Swagger
+            //services.AddSwaggerGen(o =>
+            //{
+            //    o.SwaggerDoc("v1", new Info
+            //    {
+            //        Title = "MyBooks API",
+            //        Version = "v1",
+            //        Contact = new Contact
+            //        {
+            //            Email = "toth.dani9204@gmail.com",
+            //            Name = "Daniel Toth"
+            //        }
+            //    });
+            //    o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "MyBooks.Api.xml"));
+            //    o.DescribeAllEnumsAsStrings();
+            //});
+
+            // Problem details
+            services.AddProblemDetails(options =>
+            {
+                options.IncludeExceptionDetails = ctx => false;
+                options.Map<EntityNotFoundException>(ex => new ProblemDetails
+                {
+                    Title = "Invalid ID",
+                    Status = StatusCodes.Status404NotFound
+                });
+            });
+
+            // DBContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                //options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection"));
+                options.UseNpgsql(Configuration.GetConnectionString("NpgSqlServerConnection"));
+            }); 
+
+            // Health checks
+            services.AddHealthChecks()
+                .AddDbContextCheck<ApplicationDbContext>();
+
+            // Mvc
+            services.AddMvc(o => o.MaxModelValidationErrors = 50)
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+            // API versioning
+            services.AddApiVersioning(options =>
+            {
+                options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+            });
+
+            // Authentication
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = "http://localhost:5001";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "myBooksApi";
+                    options.ApiSecret = "secret";
+                });
+
+            // Authorization
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(
+                    "AdminOnly",
+                    policy => policy.RequireClaim("Level", "Admin"));
+            });
+
+            // Services
+            services.AddSingleton(MapperConfig.Configure());
+            services.AddTransient<IBookService, BookService>();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="context"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    //app.UseHsts();
+            //    app.UseExceptionHandler("/error");
+            //}
+
+            //app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseHealthChecks("/health");
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyBooks API v1"));
+
+            app.UseAuthentication();
+
+            app.UseProblemDetails();
+            app.UseRouting();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            //context.Seed();
+        }
+    }
+}
