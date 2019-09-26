eed0b0b45e88dfb90426836f124f20cc2459523a
Daniel <toth.dani92@hotmail.com>
2019-09-26 17:29:13 +0200

    Add Microsoft.Extensions.Hosting support

--- src/MyBooks.Client.Wpf/App.xaml.cs
+++ src/MyBooks.Client.Wpf/App.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MyBooks.Client.Infrastructure;
+using Serilog;
 
 namespace MyBooks.Client.Wpf
 {
@@ -7,11 +12,41 @@ namespace MyBooks.Client.Wpf
     /// </summary>
     public partial class App : Application
     {
-        public static AppBootstrapper BootStrapper;
+        /// <summary>
+        /// IHost
+        /// </summary>
+        private IHost _host;
 
-        public App()
+        /// <summary>
+        /// Overridden event handler on application startup.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void OnApplicationStartup(object sender, StartupEventArgs e)
         {
-            BootStrapper = new AppBootstrapper();
+            _host = HostBuilderConfig.Configure(e.Args).Build();
+
+            var logger = _host.Services.GetRequiredService<ILogger<App>>();
+            logger.LogInformation("The application has started successfully.");
+
+            await _host.StartAsync();
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        /// <summary>
+        /// Overridden event handler on application exit.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void OnApplicationExit(object sender, ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync();
+                Log.CloseAndFlush();
+            }
         }
     }
 }
