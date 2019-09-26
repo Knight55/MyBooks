0b139ceb77ca8844de27ebc51c22b6d0afefd8f5
Daniel <toth.dani92@hotmail.com>
2019-09-26 17:27:00 +0200

    Removed unnecessary using statements

--- src/MyBooks.Client.Wpf/Views/NewBookWindow.xaml.cs
+++ src/MyBooks.Client.Wpf/Views/NewBookWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Disposables;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Reactive.Disposables;
 using MyBooks.Client.ViewModels;
 using ReactiveUI;
-using Splat;
 
 namespace MyBooks.Client.Wpf.Views
 {
@@ -26,7 +12,6 @@ namespace MyBooks.Client.Wpf.Views
         public NewBookWindow()
         {
             InitializeComponent();
-            ViewModel = Locator.Current.GetService<NewBookViewModel>();
 
             this.WhenActivated(disposableRegistration =>
             {
