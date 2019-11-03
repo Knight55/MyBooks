5f8b73fdb606c4ede65a55bed7234c36a34f6521
Daniel <toth.dani92@hotmail.com>
2019-11-03 22:27:22 +0100

    Add dto for ratings (#3)

---
+++ src/MyBooks.Dto/Dtos/Rating.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBooks.Dto.Dtos
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+
+        public int Value { get; set; }
+
+        public string Text { get; set; }
+    }
+}
