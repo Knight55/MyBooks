46a94a8b5542af2aa983e0200072d2e0d13da008
Daniel <toth.dani92@hotmail.com>
2019-11-03 22:25:19 +0100

    Add exception for Goodreads API (#3)

---
+++ src/MyBooks.Api/GoodreadsEntityNotFoundException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyBooks.Api
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GoodreadsEntityNotFoundException : Exception
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public GoodreadsEntityNotFoundException()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public GoodreadsEntityNotFoundException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public GoodreadsEntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
