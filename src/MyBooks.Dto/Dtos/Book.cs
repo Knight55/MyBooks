6deb89b26ead1e006664063df5924f40f27b1f75
Daniel <toth.dani92@hotmail.com>
2019-11-03 22:28:57 +0100

    Modified constructors from Goodreads entities (#3)

--- src/MyBooks.Dto/Dtos/Book.cs
+++ src/MyBooks.Dto/Dtos/Book.cs
@@ -1,31 +1,44 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
+using MyBooks.Dto.Goodreads;
 
 namespace MyBooks.Dto.Dtos
 {
     public class Book
     {
-        public Book()
-        {
-        }
+        public Book() {}
 
-        public Book(Work work)
+        public Book(GoodreadsWork work)
         {
+            GoodreadsId = work.Book.Id;
             Title = work.Book.Title;
-            CoverUrl = work.Book.ImageUrl;
-            Rating = double.Parse(work.AverageRating, NumberStyles.AllowDecimalPoint, NumberFormatInfo.InvariantInfo);
+            CoverImageUrl = work.Book.ImageUrl;
         }
 
+        public Book(GoodreadsBook book)
+        {
+            Isbn = book.Isbn;
+            GoodreadsId = book.Id;
+            Title = book.Title;
+            CoverImageUrl = book.ImageUrl;
+            Summary = book.Description;
+            //foreach (var bookAuthor in book.Authors)
+            //{
+            //    AuthorIds.Add(int.Parse(bookAuthor.Id));
+            //}
+        }
+        
         public int Id { get; set; }
+
+        public string Isbn { get; set; }
+
+        public string GoodreadsId { get; set; }
 
         /// <summary>
         /// The title of the book.
         /// </summary>
-        [Required(ErrorMessage = "Book title is required", AllowEmptyStrings = false)]
         public string Title { get; set; }
 
-        public string CoverUrl { get; set; }
+        public string CoverImageUrl { get; set; }
 
         public string Summary { get; set; }
 
@@ -35,6 +48,6 @@ namespace MyBooks.Dto.Dtos
 
         public string GoodreadsUrl { get; set; }
 
-        public List<int> AuthorIds { get; set; }
+        public List<int> AuthorIds { get; set; } = new List<int>();
     }
 }
