3ec015624c46ee00041bfb00fb466ab6afcd21d2
Daniel <toth.dani92@hotmail.com>
2019-11-06 22:48:58 +0100

    Implemented UserManagerService for handling users (#2)

--- src/MyBooks.Client/Services/IUserManagerService.cs
+++ src/MyBooks.Client/Services/IUserManagerService.cs
@@ -1,7 +1,12 @@
-﻿namespace MyBooks.Client.Services
+﻿using System.Threading.Tasks;
+using IdentityModel.OidcClient;
+
+namespace MyBooks.Client.Services
 {
     public interface IUserManagerService
     {
-        
+        LoginResult LoginResult { get; set; }
+
+        Task Login();
     }
 }
---
+++ src/MyBooks.Client/Services/UserManagerService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace MyBooks.Client.Services
+{
+    public class UserManagerService : IUserManagerService
+    {
+        private readonly ILogger<UserManagerService> _logger;
+        private readonly OidcClientOptions _oidcClientOptions;
+
+        private readonly OidcClient _oidcClient;
+
+        public LoginResult LoginResult { get; set; }
+
+        public UserManagerService(
+            ILogger<UserManagerService> logger,
+            IOptions<OidcClientOptions> oidcClientOptions)
+        {
+            _logger = logger;
+            _oidcClientOptions = oidcClientOptions.Value;
+            _oidcClient = new OidcClient(_oidcClientOptions);
+        }
+
+        public async Task Login()
+        {
+            try
+            {
+                LoginResult = await _oidcClient.LoginAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Unexpected Error: {ex.Message}");
+                return;
+            }
+
+            if (LoginResult.IsError)
+            {
+                if (LoginResult.Error == "UserCancel")
+                {
+                    _logger.LogError("The sign-in window was closed before authorization was completed.");
+                }
+                else
+                {
+                    _logger.LogInformation($"Error: {LoginResult.Error}");
+                }
+            }
+            else
+            {
+                var name = LoginResult.User.Identity.Name;
+                _logger.LogInformation($"User {name} logged in successfully.");
+            }
+        }
+    }
+}
